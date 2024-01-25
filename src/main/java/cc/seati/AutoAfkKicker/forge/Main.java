3b5cf2292c34492bd985530ced571c1f0adcbd0f
Subilan <suyuanliang1995@gmail.com>
2024-01-25 15:23:59 +0800

    register

--- src/main/java/cc/seati/AutoAfkKicker/forge/Main.java
+++ src/main/java/cc/seati/AutoAfkKicker/forge/Main.java
@@ -1,9 +1,13 @@
 package cc.seati.AutoAfkKicker.forge;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
@@ -11,11 +15,12 @@ import org.apache.logging.log4j.Logger;
 
 @Mod("autoafkkicker")
 public class Main {
+    public static final String MOD_ID = "autoafkkicker";
     public static final Logger LOGGER = LogManager.getLogger();
     public Main() {
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
-        Utils.registerListener(Events::onPlayerJoin);
-        Utils.registerListener(Events::onPlayerQuit);
+        DistExecutor.safeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.GENERAL_SPEC, "autoafkkicker.toml"));
+        DistExecutor.safeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup));
     }
 
     private void setup(final FMLCommonSetupEvent e) {
