faee3e98a80f2ac62a24b628b6e27a59668289ad
Subilan <suyuanliang1995@gmail.com>
2024-01-25 15:24:25 +0800

    event logic

--- src/main/java/cc/seati/AutoAfkKicker/forge/Events.java
+++ src/main/java/cc/seati/AutoAfkKicker/forge/Events.java
@@ -1,13 +1,30 @@
 package cc.seati.AutoAfkKicker.forge;
 
+import cc.seati.AutoAfkKicker.forge.Timers.PlayerTickTimer;
+import cc.seati.AutoAfkKicker.forge.Timers.PlayerTickTimerStorage;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.DEDICATED_SERVER)
 public class Events {
+    @SubscribeEvent
     public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent e) {
-
+        String playername = e.getPlayer().getGameProfile().getName();
+        PlayerTickTimer timer  = PlayerTickTimer.createFor(playername);
+        timer.setWorking(true);
+        PlayerTickTimerStorage.addTimerForName(timer, playername);
     }
 
+    @SubscribeEvent
     public static void onPlayerQuit(PlayerEvent.PlayerLoggedOutEvent e) {
+        PlayerTickTimerStorage.removeTimerForName(e.getPlayer().getGameProfile().getName());
+    }
 
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent e) {
+        if (e.phase == TickEvent.Phase.END) InactivityWatcher.judge(e.player);
     }
 }
